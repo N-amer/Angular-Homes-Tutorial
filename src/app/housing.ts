a47205c5fb50cbc9a85e684412651f1f7778a882
Nooman Amer <nooman178@gmail.com>
2025-06-27 14:27:07 +0200

    updated the app to add a form using angulars forms feature

--- src/app/housing.ts
+++ src/app/housing.ts
@@ -114,7 +114,9 @@ export class Housing {
   getHousingLocationById(id: number): HousingLocationInfo | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
-  constructor() { }
-
-
+ submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+  }
 }
